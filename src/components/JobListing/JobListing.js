837349a501470830717d59b0b19e202aba1516f9
Ruhani Makhija <ruhani.makhija@brightchamps.com>
2024-05-05 18:59:34 +0530

    implemented inifinite loading

--- src/components/JobListing/JobListing.js
+++ src/components/JobListing/JobListing.js
@@ -4,36 +4,55 @@ import "./jobListing.css"
 
 const JobListing = () => {
     const [jobListings, setJobListings] = useState([]);
+    const [offset,setOffset]=useState(0);
+
+    //fetehced  api data
+    const fetchJobListings = async (offset) => {
+        try {
+            const myHeaders = new Headers();
+            myHeaders.append("Content-Type", "application/json");
+    
+            const body = JSON.stringify({
+            "limit": 10,
+            "offset": offset
+            });
+    
+            const requestOptions = {
+            method: "POST",
+            headers: myHeaders,
+            body
+            };
+    
+            const response = await fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions);
+            const jsonData = await response.json();
+            setJobListings((prevJobListing)=>[...prevJobListing,...jsonData?.jdList]);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+
+    //implemented scroll event
     useEffect(()=>{
-        const fetchJobListings = async (offset) => {
-
-            try {
-              const myHeaders = new Headers();
-              myHeaders.append("Content-Type", "application/json");
-      
-              const body = JSON.stringify({
-                "limit": 10,
-                "offset": 0
-              });
-      
-              const requestOptions = {
-                method: "POST",
-                headers: myHeaders,
-                body
-              };
-      
-              const response = await fetch("https://api.weekday.technology/adhoc/getSampleJdJSON", requestOptions);
-              const jsonData = await response.json();
-              setJobListings((prevJobListing)=>[...prevJobListing,...jsonData?.jdList]);
-              
-              console.log(jsonData);
-            } catch (error) {
-              console.error(error);
-            }
-          };
-
-          fetchJobListings();
+        fetchJobListings(offset);
+        window.addEventListener("scroll",handleScroll);
+        return ()=>  window.removeEventListener("scroll",handleScroll);
     },[])
+
+
+    //implemented infinite scroll condition
+    const handleScroll=()=>{
+        if(window.scrollY+window.innerHeight+0.5>=document.body.scrollHeight){
+            setOffset((prevOffset) => prevOffset + 10);
+        }
+    }
+
+    //when offsets changes fetch more data
+    useEffect(()=>{
+        fetchJobListings(offset);
+      },[offset])
+
+      
   return (
     <div className='job-listing-container'>
        {jobListings.map((job, index) => (
